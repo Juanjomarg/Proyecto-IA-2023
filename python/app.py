3b9ae64fd33751f0a73b7d120f04eeaead6d736e
juanjomarg <juanj.martinezg96@gmail.com>
2023-09-14 18:16:06 -0500

    aplicación básica

---
+++ python/app.py
@@ -0,0 +1,24 @@
+from libraries import *
+
+app = dash.Dash(
+    __name__,
+    external_stylesheets=[dbc.themes.COSMO, dbc.icons.FONT_AWESOME],
+    use_pages=True
+)
+server=app.server
+
+app.layout = dbc.Container(
+    [
+        navbar,
+        sidebar,
+        html.Div(
+            [
+                dash.page_container
+            ],
+            className="content",
+        ),
+    ]
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
---
+++ python/libraries.py
@@ -0,0 +1,19 @@
+
+
+
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+
+
+import dash
+import dash_bootstrap_components as dbc
+from dash import html,Input,Output,State,dcc,dash_table
+-
+import sklearn
+from sklearn import datasets  
+from sklearn.model_selection import train_test_split  # Import a specific function
+
+
+
+from python.comps import *
